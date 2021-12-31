0a24e63aeca944e912f68bb71d7141225e7d3ba4
devaraj reddy <devaraj reddy@DESKTOP-QP6PFOL>
2021-12-31 14:36:41 +0530

    commit demo

---
+++ sdet27/src/test/java/practice/Createcontact.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Createcontact {
+	
+	@Test
+	public void createcont() {
+		System.out.println("contact create successufully");
+	}
+	
+
+}
---
+++ sdet27/src/test/java/practice/CreatepasswordTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class CreatepasswordTest {
+	@Test
+	public void createPwd() {
+		System.out.println("create password successufully");
+	}
+
+}
